343b83470939f11b5d1763e32fbaa54457a9248c
SakamotoShun <sohshunhong@hotmail.com>
2021-10-19 19:03:02 +0800

    created exe

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import math
 from queue import PriorityQueue
 
-WIDTH = 800
+WIDTH = 1000
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Path Finding Algorithm")
 
